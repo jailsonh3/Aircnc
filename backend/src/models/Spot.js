4de55121c57cfc9bb7d4e9a5acb7a81c8e630c16
Jailsonk8 <jailsonk8@hotmail.com>
2019-11-16 20:51:17 -0300

    upload project aircnc

---
+++ backend/src/models/Spot.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const SpotShema = new mongoose.Schema({
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+ }, {
+     toJSON: {
+         virtuals: true,
+     }
+ });
+
+ SpotShema.virtual('thumbnail_url').get(function() {
+     return `http://192.168.0.6:3333/files/${this.thumbnail}` 
+ })
+
+module.exports = mongoose.model('Spot', SpotShema);
---
+++ mobile/src/pages/List.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage, TouchableOpacity } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List({ navigation }) {
+
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.0.6:3333', {
+                query: { user_id },
+            });
+
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+            })
+        })
+
+    }, []);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    async function handleLogout() {
+
+        await AsyncStorage.clear();
+        
+        navigation.navigate('Login');
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={() => handleLogout()}>
+                <Image style={styles.logo} source={logo} />
+            </TouchableOpacity>
+           
+    
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 25,
+    },
+
+
+});
